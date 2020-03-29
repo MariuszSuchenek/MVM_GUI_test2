969c79f4d7a3ec8cd931d64a7887ae40c8282dad
javier <javier.romualdez@gmail.com>
2020-03-29 17:47:04 -0400

    Finished preliminary functionality for toolsettings widget

--- gui/toolsettings/toolsettings.py
+++ gui/toolsettings/toolsettings.py
@@ -6,7 +6,25 @@ class ToolSettings(QtWidgets.QWidget):
     def __init__(self, *args):
         super(ToolSettings, self).__init__(*args)
         uic.loadUi("toolsettings/toolsettings.ui", self)
+        self.label_name = self.findChild(QtWidgets.QLabel, "label_name")
+        self.label_value = self.findChild(QtWidgets.QLabel, "label_value")
+        self.slider_value = self.findChild(QtWidgets.QSlider, "slider_value")
+
+        self.slider_value.valueChanged.connect(self.update)
+
         self.show()
     def setup(self, name, setrange):
-        print(1)
+        self.label_name.setText(name)
+
+        # unpack and assign slider min, current, and max
+        (low, val, high) = setrange
+        self.slider_value.setMinimum(low)
+        self.slider_value.setMaximum(high)
+        self.update(val)
+        print("Set value to " + str(low) + " " + str(val)  + " " + str(high))
+
+    def update(self, value):
+        self.slider_value.setSliderPosition(value)
+        self.label_value.setText(str(value))
+
 
