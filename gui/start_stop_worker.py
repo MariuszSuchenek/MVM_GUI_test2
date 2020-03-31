f08aad44e4b9d477ad5695cd174d45f085a75c74
javier <javier.romualdez@gmail.com>
2020-03-31 15:46:56 -0400

    Modified behavior to have mode (auto or assist) selected and start/stop toggleable from separate button; fixed apparent bug where serial esp32 commands would be sent to stop even without confirmation

--- gui/start_stop_worker.py
+++ gui/start_stop_worker.py
@@ -15,45 +15,56 @@ class StartStopWorker():
     DO_RUN = 1
     DONOT_RUN = 0
 
-    def __init__(self, main_window, config, esp32, button_startauto, button_startman):
+    def __init__(self, main_window, config, esp32, button_startstop, button_autoassist):
         self.main_window = main_window
         self.config = config
         self.esp32 = esp32
-        self.button_startauto = button_startauto
-        self.button_startman = button_startman
+        self.button_startstop = button_startstop
+        self.button_autoassist = button_autoassist
 
         self.mode = self.MODE_STOP
+        self.desired_mode = self.MODE_AUTO
         return 
 
-    def start_button_pressed(self, button):
-        self.button_startman.setDisabled(True)
-        self.button_startauto.setEnabled(False)
-        self.button_startman.repaint()
-        self.button_startauto.repaint()
-        text = button.text()
-        text = text.replace('Start', 'Stop')
-        button.setText(text)
+    def toggle_mode(self):
+        if self.desired_mode == self.MODE_AUTO:
+            self.desired_mode = self.MODE_ASSIST
+            self.button_autoassist.setText("Assisted")
+        else:
+            self.desired_mode = self.MODE_AUTO
+            self.button_autoassist.setText("Automatic")
 
-    def stop_button_pressed(self, button):
-        button.setDown(False)
-        currentMode = button.text().split(' ')[1].upper()
+    def start_button_pressed(self):
+        self.button_startstop.setDisabled(True)
+        self.button_autoassist.setDisabled(True)
+        self.button_startstop.repaint()
+        self.button_autoassist.repaint()
+
+        self.button_startstop.setText("Stop")
+        QtCore.QTimer.singleShot(self.button_timeout(), lambda: ( 
+                 self.button_startstop.setEnabled(True),
+                 self.button_startstop.setStyleSheet("color: red")))
+
+    def stop_button_pressed(self):
+        self.button_startstop.setEnabled(True)
+        self.button_autoassist.setEnabled(True)
+        self.button_startstop.repaint()
+        self.button_autoassist.repaint()
+
+        self.button_startstop.setText("Start")
+        self.button_startstop.setStyleSheet("color: black")
+
+    def confirm_stop_pressed(self):
+        self.button_autoassist.setDown(False)
+        currentMode = self.button_autoassist.text().upper()
         confirmation = QtWidgets.QMessageBox.warning(
             self.main_window, 
-            '**STOPPING AUTOMATIC MODE**', 
+            '**STOPPING ' + currentMode + ' MODE**', 
             "Are you sure you want to STOP " + currentMode + " MODE?", 
             QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel, 
             QtWidgets.QMessageBox.Cancel)
-        
-        if confirmation == QtWidgets.QMessageBox.Ok:
-            self.mode = self.MODE_STOP
-            text = button.text()
-            text = text.replace('Stop', 'Start')
-            button.setText(text)
-            button.setStyleSheet("color: black")
-            self.button_startauto.setEnabled(True)
-            self.button_startman.setEnabled(True)
-            self.button_startman.repaint()
-            self.button_startauto.repaint()
+        return confirmation == QtWidgets.QMessageBox.Ok
+
 
     def button_timeout(self):
         print('Setting timeout')
@@ -66,63 +77,32 @@ class StartStopWorker():
                 timeout = 3000
         return timeout
 
-    def toggle_automatic(self):
+    def toggle_start_stop(self):
         """
-        Toggles between automatic mode (1) and stop (0).
-
-        Changes text from "Start" to "Stop" and en/disables assisted button depending on mode.
+        Toggles between desired mode (MODE_ASSIST or MODE_AUTO) and MODE_STOP.
         """
 
         print('Current mode: {}'.format(self.mode))
         if self.mode == self.MODE_STOP:
-            self.mode = self.MODE_AUTO
+            self.mode = self.desired_mode
 
             # Send signal to ESP to start automatic mode
-            result = self.esp32.set('mode', self.MODE_AUTO)
-            result = self.esp32.set('run', self.DO_RUN)
-
-            self.start_button_pressed(self.button_startauto)
-
-            QtCore.QTimer.singleShot(self.button_timeout(), lambda: ( 
-                     self.button_startauto.setDisabled(False),
-                     self.button_startauto.setStyleSheet("color: red")))
-
-        else:
-            # Send signal to ESP to stop running
-            self.esp32.set('run', self.DONOT_RUN)
-            self.esp32.set('mode', self.MODE_STOP)
-
-            self.stop_button_pressed(self.button_startauto)
-
-
-    def toggle_assisted(self):
-        """
-        Toggles between assisted mode (2) and stop (0).
-
-        Changes text from "Start" to "Stop" and en/disables automatic button depending on mode.
-        """
-
-        if self.mode == self.MODE_STOP:
-            self.mode = self.MODE_ASSIST
-
-            # Send signal to ESP to start automatic mode
-            result = self.esp32.set('mode', self.MODE_ASSIST)
+            result = self.esp32.set('mode', self.mode)
             result = self.esp32.set('run', self.DO_RUN)
 
             if result != self.config['return_success_code']:
                 print(f"\033[91mERROR: Failed to start with mode AUTOMATIC.\033[0m")
 
-            self.start_button_pressed(self.button_startman)
-            
-            QtCore.QTimer.singleShot(self.button_timeout(), lambda: ( 
-                    self.button_startman.setDisabled(False),
-                    self.button_startman.setStyleSheet("color: red")))
+            self.start_button_pressed()
+
 
         else:
-            # Send signal to ESP to stop running
-            self.esp32.set('run', self.DONOT_RUN)
-            self.esp32.set('mode', self.MODE_STOP)
+            if self.confirm_stop_pressed():
+                self.mode = self.MODE_STOP
+                
+                # Send signal to ESP to stop running
+                self.esp32.set('run', self.DONOT_RUN)
+                self.esp32.set('mode', self.MODE_STOP)
 
-            self.stop_button_pressed(self.button_startman)
+                self.stop_button_pressed()
 
-      
