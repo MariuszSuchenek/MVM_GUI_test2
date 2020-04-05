0644dbcdf58139528c072a97010cbdcf251ae6d1
Marco Del Tutto <marco.deltutto@gmail.com>
2020-04-04 22:00:11 -0500

    Adding a setup script that checks if all the packages are installed and sets up the MVM base dir

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+MVMGUI_BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Test numpy
+if ! $(python -c "import numpy" &> /dev/null); then 
+  echo "ERROR: can not use the MVM GUI due to missing package numpy."
+  return
+fi
+
+# Test pyqt5
+if ! $(python -c "import PyQt5" &> /dev/null); then 
+  echo "ERROR: can not use the MVM GUI due to missing package PyQt5."
+  return
+fi
+
+# Test pyqtgraph
+if ! $(python -c "import pyqtgraph" &> /dev/null); then 
+  echo "ERROR: can not use the MVM GUI due to missing package pyqtgraph."
+  return
+fi
+
+# Test pyserial
+if ! $(python -c "import serial" &> /dev/null); then 
+  echo "ERROR: can not use the MVM GUI due to missing package pyserial."
+  return
+fi
+
+# Test pyyaml
+if ! $(python -c "import yaml" &> /dev/null); then 
+  echo "ERROR: can not use the MVM GUI due to missing package pyyaml."
+  return
+fi
+
+
+export PYTHONPATH=$MVMGUI_BASEDIR/gui:$PYTHONPATH
+echo $PYTHONPATH
