370b02e85bcb11bf1ed8c06ed366c63d146261fa
javier <javier.romualdez@gmail.com>
2020-04-04 21:54:00 -0400

    Added maindisplay widget to govern main pane above bottombar

---
+++ gui/maindisplay/maindisplay.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from PyQt5 import QtWidgets, uic
+from PyQt5 import QtGui
+import sys
+
+class MainDisplay(QtWidgets.QWidget):
+    def __init__(self, *args):
+        """
+        Initialize the MainDisplay container widget.
+
+        Provides a passthrough to underlying widgets.
+        """
+        super(MainDisplay, self).__init__(*args)
+        uic.loadUi("maindisplay/maindisplay.ui", self)
