487ac49997e19a67f1d18cbb9278936c8783b5c4
Marco Del Tutto <marco.deltutto@gmail.com>
2020-04-04 22:24:47 -0500

    Clean up

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,19 +1,9 @@
+#!/usr/bin/env python3
 
 from pytestqt import qt_compat
 from pytestqt.qt_compat import qt_api
 
 from mvm_basics import *
-# from PyQt5 import QtCore, QtGui, QtWidgets
-# import os, sys, yaml
-
-# from communication.fake_esp32serial import FakeESP32Serial
-
-
-# base_dir = os.path.dirname(__file__)
-# settings_file = os.path.join(base_dir, 'default_settings.yaml')
-# with open(settings_file) as f:
-#     config = yaml.load(f, Loader=yaml.FullLoader)
-# esp32 = FakeESP32Serial()
 
 def test_basics(qtbot):
     '''
@@ -44,8 +34,7 @@ def test_menu(qtbot):
     qtbot.mouseClick(window.button_menu, QtCore.Qt.LeftButton)
     assert window.bottombar.currentIndex() == 1
 
-if __name__ == "__main__":
-    test_hello()
+
 
 
 
