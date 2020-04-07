5e67712334a90627f826061b8a3c8e6abd9c00fa
Carmelo Pellegrino <carmelo.pellegrino@gmail.com>
2020-04-08 00:47:25 +0200

    MessageBox class rewritten

--- gui/messagebox.py
+++ gui/messagebox.py
@@ -1,124 +1,60 @@
+from functools import reduce
 from PyQt5.QtWidgets import QMessageBox
 
-class MessageBox():
+class MessageBox(QMessageBox):
     '''
-    Class that helps display a QT message box, with callbacks
+    Class that displaes a QT message box, with callbacks
     attached to each button.
 
     Example usage:
 
     ```
-    from PyQt5.QtWidgets import QMessageBox
+    from messagebox import MessageBox
 
     callbacks = {QMessageBox.Retry: your_retry_callback_function,
-                 QMessageBox.Abort: exit}
+                 QMessageBox.Abort: lambda: sys.exit(-1)}
 
-    MessageBox().critical("Title", "Text", callbacks)
+    MessageBox().critical("short message", "long message", "details",
+                          "Window title", callbacks)
     ```
     '''
+
     def __init__(self):
-        self.msg = QMessageBox()
-        self.callbacks = {}
+        """
+        Constructor
 
-    def confirm(self, window_title, text, info_text=None, ok_callback=None, cancel_callback=None):
-        '''
-        Show a confirmation dialog with OK and Cancel buttons.
+        Create a new MessageBox instance.
+        """
 
-        You may specify callback functions for each button, or just check
-        the return value of this function (OK => True, Cancel => False).
-        '''
-        self.callbacks = {QMessageBox.Ok: ok_callback,
-                          QMessageBox.Cancel: cancel_callback}
+        super(MessageBox, self).__init__()
 
-        btn = self._show(title, text, info_text, QMessageBox.Question, QMessageBox.Cancel)
-        return btn == QMessageBox.Ok
 
-    def question(self, window_title, text, info_text, button_callbacks, default_button=None):
-        '''
-        Show a question dialog.
+    def _wrapper(self, text, long_text, detail_text, title, icon, callbacks):
+        """
+        Wrapper function to implement the message box
 
-        button_callbacks is a dictionary where the keys are
-        QMessageBox.StandardButton, and the values are functions to call
-        if that button is pressed (or None).
+        arguments:
+        - text          the short message
+        - long_text     the long message
+        - detail_text   the text to be shown in the optional text box
+                        (None for none)
+        - title         the window title
+        - icon          one in:
+                            MessageBox.Question, MessageBox.Information,
+                            MessageBox.Warning, and MessageBox.Critical
+        - callbacks     dictionary: keys are one in:
+                            MessageBox.Ok, MessageBox.Open, MessageBox.Save,
+                            MessageBox.Cancel, MessageBox.Close,
+                            MessageBox.Yes, MessageBox.No, MessageBox.Abort,
+                            MessageBox.Retry, and MessageBox.Ignore
+                        value is a user-defined function object
 
-        default_button is the QMessageBox.StandardButton that is
-        highlighted by default.
-        '''
-        self.callbacks = button_callbacks
-        self._show(title, text, QMessageBox.Question, default_button)
+        returns the function object corresponding to the clicked button.
+        """
 
-    def message(self, title, text, info_text=None, ok_callback=None):
-        '''
-        Show a message dialog with an OK button.
-
-        You may specify a callback function that is called when the
-        user presses the OK button.
-        '''
-        self.callbacks = {QMessageBox.Ok: ok_callback}
-        self._show(title, text, info_text, QMessageBox.Information, QMessageBox.Ok)
-
-    def warning(self, title, text, info_text=None, button_callbacks={QMessageBox.Ok: None}, default_button=None, detail=None):
-        '''
-        Show a warning dialog.
-
-        button_callbacks is a dictionary where the keys are
-        QMessageBox.StandardButton, and the values are functions to call
-        if that button is pressed (or None).
-
-        default_button is the QMessageBox.StandardButton that is
-        highlighted by default.
-
-        detail is optional text that will be displayed if the user
-        clicks a "More details" button.
-        '''
-        self.callbacks = button_callbacks
-        self._show(title, text, info_text, QMessageBox.Warning, default_button, detail)
-
-    def critical(self, title, text, info_text=None, button_callbacks={QMessageBox.Ok: None}, default_button=None, detail=None):
-        '''
-        Show a critical warning dialog.
-
-        button_callbacks is a dictionary where the keys are
-        QMessageBox.StandardButton, and the values are functions to call
-        if that button is pressed (or None).
-
-        default_button is the QMessageBox.StandardButton that is
-        highlighted by default.
-
-        detail is optional text that will be displayed if the user
-        clicks a "More details" button.
-        '''
-        self.callbacks = button_callbacks
-
-        # We don't set a stylesheet on the messagebox, as setting one attribute
-        # requires you to redefine every attribute so that it matches the look
-        # of the default widgets. Instead, we can set text properties using tags.
-        if text and len(text):
-            text = "<font color='#ff0000'>%s</font>" % text
-
-        self._show(title, text, info_text, QMessageBox.Critical, default_button, detail)
-
-    def _show(self, title, text, info_text, icon, default_btn=None, detail_text=""):
-        '''
-        Build the actual message box, based on the supplied
-        arguments and the content of self.callbacks.
-        '''
-        self.msg.setIcon(icon)
-
-        self.msg.setWindowTitle(title)
-
-        # Make text bigger. Simplest to do using tags rather than QMessageBox stylesheet.
-        if text and len(text):
-            text = "<big>%s</big>" % text
-
-        if info_text and len(info_text):
-            info_text = "<big>%s</big>" % info_text
-
-        self.msg.setText(text)
-        self.msg.setInformativeText(info_text)
-        self.msg.setDetailedText(detail_text)
-
-        # Make the buttons bigger
+        text = "<big>%s</big>" % text
+        long_text = "<big>%s</big>" % long_text
+        bitmask = reduce(lambda o, x: o | x, callbacks)
         button_style = """
         QPushButton {
             border: 1px solid #8f8f91;
@@ -141,24 +77,84 @@ class MessageBox():
         QPushButton:default {
             border: 2px solid navy;
         }"""
-        self.msg.setStyleSheet(button_style);
 
-        bitmask = 0
-        for b in self.callbacks.keys():
-            bitmask |= b
+        self.setIcon(icon)
+        self.setWindowTitle(title)
+        self.setText(text)
+        self.setInformativeText(long_text)
+        self.setDetailedText(detail_text)
+        self.setStyleSheet(button_style);
+        self.setStandardButtons(bitmask)
 
-        self.msg.setStandardButtons(bitmask)
+        return callbacks[self.exec()]
 
-        if default_btn:
-            self.msg.setDefaultButton(default_btn)
+    def question(self, text, long_text, detail_text, title, callbacks):
+        """
+        Display a question message window
 
-        btn = self.msg.exec()
-        self._click_handler(btn)
-        return btn
+        arguments:
+        - text          the short message
+        - long_text     the long message
+        - detail_text   the text to be shown in the optional text box
+                        (None for none)
+        - title         the window title
+        - callbacks     dictionary: keys are one in:
+                            MessageBox.Ok, MessageBox.Open, MessageBox.Save,
+                            MessageBox.Cancel, MessageBox.Close,
+                            MessageBox.Yes, MessageBox.No, MessageBox.Abort,
+                            MessageBox.Retry, and MessageBox.Ignore
+                        value is a user-defined function object
 
-    def _click_handler(self, btn):
-        callback = self.callbacks.get(btn, None)
+        returns the function object corresponding to the clicked button.
+        """
+        return self._wrapper(text, long_text, detail_text, title,
+                             self.Question, callbacks)
 
-        if callback:
-            callback()
+    def critical(self, text, long_text, detail_text, title, callbacks):
+        """
+        Display a critical error window
+
+        arguments:
+        - text          the short message
+        - long_text     the long message
+        - detail_text   the text to be shown in the optional text box
+                        (None for none)
+        - title         the window title
+        - callbacks     dictionary: keys are one in:
+                            MessageBox.Ok, MessageBox.Open, MessageBox.Save,
+                            MessageBox.Cancel, MessageBox.Close,
+                            MessageBox.Yes, MessageBox.No, MessageBox.Abort,
+                            MessageBox.Retry, and MessageBox.Ignore
+                        value is a user-defined function object
+
+        returns the function object corresponding to the clicked button.
+        """
+
+        text = "<font color='#ff0000'>%s</font>" % text
+        return self._wrapper(text, long_text, detail_text, title,
+                             self.Critical, callbacks)
+
+    def warning(self, text, long_text, detail_text, title, callbacks):
+        """
+        Display a warning message window
+
+        arguments:
+        - text          the short message
+        - long_text     the long message
+        - detail_text   the text to be shown in the optional text box
+                        (None for none)
+        - title         the window title
+        - callbacks     dictionary: keys are one in:
+                            MessageBox.Ok, MessageBox.Open, MessageBox.Save,
+                            MessageBox.Cancel, MessageBox.Close,
+                            MessageBox.Yes, MessageBox.No, MessageBox.Abort,
+                            MessageBox.Retry, and MessageBox.Ignore
+                        value is a user-defined function object
+
+        returns the function object corresponding to the clicked button.
+        """
+
+        return self._wrapper(text, long_text, detail_text, title,
+                             self.Warning, callbacks)
+
 
