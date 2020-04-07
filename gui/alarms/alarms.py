eeefa21b52d71004e2702802c4068ff29ed81b6d
javier <javier.romualdez@gmail.com>
2020-04-07 10:14:42 -0400

    Alarms panel now displays unused monitors and has options for setting limits, stats, and adding to the main display

--- gui/alarms/alarms.py
+++ gui/alarms/alarms.py
@@ -12,3 +12,18 @@ class Alarms(QtWidgets.QWidget):
         super(Alarms, self).__init__(*args)
         uic.loadUi("alarms/alarms.ui", self)
 
+        self.layout = self.findChild(QtWidgets.QGridLayout, "monitors_layout")
+
+    def connect_monitors(self, monitors, monitor_slots):
+        self.monitors = monitors
+        self.monitor_slots = monitor_slots
+
+    def populate_monitors(self):
+        for (i, name) in enumerate(self.monitors):
+            monitor = self.monitors[name]
+            self.layout.addWidget(self.monitors[name], int(i % 3), 10-int(i / 3)) 
+            for barname in self.monitor_slots:
+                monitor_slot = self.monitor_slots[barname]
+                if monitor.location == barname:
+                    self.monitor_slots[barname].addWidget(monitor, 0, 0)
+                    break
