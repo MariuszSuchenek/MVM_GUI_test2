05a1f3785e864be1e36f22fac34125111e421f5e
javier <javier.romualdez@gmail.com>
2020-04-07 13:35:30 -0400

    Tweaked data filler connect_monitor function again to accept single monitor and name

--- gui/data_filler.py
+++ gui/data_filler.py
@@ -166,13 +166,13 @@ class DataFiller():
 
         plot.addItem(self._looping_lines[name])
 
-    def connect_monitor(self, monitor_name, monitors):
+    def connect_monitor(self, monitor_name, monitor):
         '''
         Connect a monitor to this class by
         storing it in a dictionary
         '''
         name = self._config[monitor_name]['plot_var']
-        self._monitors[name] = monitors[monitor_name]
+        self._monitors[name] = monitor
 
         print('NORMAL: Connected monitor', monitor_name, 'with variable', name)
 
