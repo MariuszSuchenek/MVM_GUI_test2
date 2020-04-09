9eff91ce54d7738f9894617864bbd56d99185626
Marco Del Tutto <marco.deltutto@gmail.com>
2020-04-08 22:30:44 -0500

    Adding warnings to the alarm handler

--- gui/alarm_handler.py
+++ gui/alarm_handler.py
@@ -2,7 +2,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from messagebox import MessageBox
-from communication.esp32serial import ESP32Alarm
+from communication.esp32serial import ESP32Alarm, ESP32Warning
 
 
 class AlarmHandler:
@@ -12,9 +12,10 @@ class AlarmHandler:
         self._esp32 = esp32
 
         self._alarm_raised = False
+        self._warning_raised = False
 
-
-        self._msg = MessageBox()
+        self._msg_err = MessageBox()
+        self._msg_war = MessageBox()
 
         self._alarm_timer = QtCore.QTimer()
         self._alarm_timer.timeout.connect(self.handle_alarms)
@@ -24,6 +25,9 @@ class AlarmHandler:
 
     def handle_alarms(self):
 
+        #
+        # ALARMS
+        #
         esp32alarm = self._esp32.get_alarms()
         print('esp32alarm', esp32alarm)	
 
@@ -32,25 +36,58 @@ class AlarmHandler:
 
             if not self._alarm_raised:
                 self._alarm_raised = True
-                self._msg.critical("ALARM",
+                self._msg_err.critical("ALARM",
                              " - ".join(errors),
                              "\n".join(errors), 
                              "Alarm received.",
-                             { self._msg.Ok: lambda: self.ok_worker,
-                               self._msg.Abort: lambda: None},
+                             { self._msg_err.Ok: lambda: self.ok_worker('alarm'),
+                               self._msg_err.Abort: lambda: None},
                              do_not_block=True)
-                self._msg.open()
+                self._msg_err.open()
             else:
-                self._msg.setInformativeText(" - ".join(errors))
-                self._msg.setDetailedText("\n".join(errors))
+                self._msg_err.setInformativeText(" - ".join(errors))
+                self._msg_err.setDetailedText("\n".join(errors))
 
-    def ok_worker(self, btn):
 
-        self._alarm_raised = False
+        #
+        # WARNINGS
+        #
+        esp32warnings = self._esp32.get_warnings()
+        print('esp32warnings', esp32warnings) 
+
+        if esp32warnings:
+            errors = esp32warnings.strerror_all()
+
+            if not self._warning_raised:
+                self._warning_raised = True
+                self._msg_war.warning("WARNING",
+                             " - ".join(errors),
+                             "\n".join(errors), 
+                             "Alarm received.",
+                             { self._msg_war.Ok: lambda: self.ok_worker('warning'),
+                               self._msg_war.Abort: lambda: None},
+                             do_not_block=True)
+                self._msg_war.open()
+            else:
+                self._msg_war.setInformativeText(" - ".join(errors))
+                self._msg_war.setDetailedText("\n".join(errors))
+
+    def ok_worker(self, mode):
+
+        if mode not in ['alarm', 'warning']:
+            raise Exception('mode must be alarm or warning.')
+
+        if mode == 'alarm':
+            self._alarm_raised = False
+        else:
+            self._warning_raised = False
 
         # Reset the alarms in the ESP
         try:
-            self._esp32.reset_alarms()
+            if mode == 'alarm':
+                self._esp32.reset_alarms()
+            else:
+                self._esp32.reset_warnings()
         except Exception as error:
             msg = MessageBox()
             fn = msg.critical("Critical",
