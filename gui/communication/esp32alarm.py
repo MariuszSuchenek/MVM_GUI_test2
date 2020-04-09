201324e96f923edbc3fd6d6f186823b09bcf2a6d
Marco Del Tutto <marco.deltutto@gmail.com>
2020-04-08 22:15:58 -0500

    Adding ESP32Warning class

--- gui/communication/esp32alarm.py
+++ gui/communication/esp32alarm.py
@@ -1,34 +1,9 @@
 import copy
 
-class ESP32Alarm:
+class ESP32BaseAlarm:
 
     alarm_to_string = {
-        # From the ESP
-        0: "Gas pressure too low",
-        1: "Gas pressure too high",
-        2: "Internal pressure too low (internal leakage)",
-        3: "Internal pressure too high",
-        4: "Out of battery power",
-        5: "Leakage in gas circuit",
-        6: "Obstruction in idraulic circuit",
-        7: "Partial obstruction in idraulic circuit",
-        # From the GUI
-        8: "Pressure to patient mouth too low",
-        9: "Pressure to patient mouth too high",
-        10: "Inpiratory flux too low",
-        11: "Inpiratory flux too high",
-        12: "Expiratory flux too low",
-        13: "Expiratory flux too high",
-        14: "Tidal volume too low",
-        15: "Tidal volume too high",
-        16: "O2 too low",
-        17: "O2 too high",
-        18: "PEEP too low",
-        19: "PEEP too high",
-        20: "Respiratory rate too low",
-        21: "Respiratory rate too high",
-        
-        31: "System failure",
+        0: "Alarm name",
     }
 
     def __init__(self, number):
@@ -76,3 +51,44 @@ class ESP32Alarm:
             str_error.append(self.strerror(n))
 
         return str_error
+
+
+
+class ESP32Alarm(ESP32BaseAlarm):
+
+    alarm_to_string = {
+        # From the ESP
+        0: "Gas pressure too low",
+        1: "Gas pressure too high",
+        2: "Internal pressure too low (internal leakage)",
+        3: "Internal pressure too high",
+        4: "Out of battery power",
+        5: "Leakage in gas circuit",
+        6: "Obstruction in idraulic circuit",
+        7: "Partial obstruction in idraulic circuit",
+        # From the GUI
+        8: "Pressure to patient mouth too low",
+        9: "Pressure to patient mouth too high",
+        10: "Inpiratory flux too low",
+        11: "Inpiratory flux too high",
+        12: "Expiratory flux too low",
+        13: "Expiratory flux too high",
+        14: "Tidal volume too low",
+        15: "Tidal volume too high",
+        16: "O2 too low",
+        17: "O2 too high",
+        18: "PEEP too low",
+        19: "PEEP too high",
+        20: "Respiratory rate too low",
+        21: "Respiratory rate too high",
+        
+        31: "System failure",
+    }
+
+class ESP32Warning(ESP32BaseAlarm):
+
+    alarm_to_string = {
+        # From the ESP
+        0: "Oxygen sensor requires calibration",
+        1: "Disconnected from power outlet",
+    }
