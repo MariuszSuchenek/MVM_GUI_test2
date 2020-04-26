c9e97cde41d56c863746b7cfb7bb3bdf7fed9880
Carmelo Pellegrino <carmelo.pellegrino@gmail.com>
2020-04-26 11:33:32 +0200

    unused imports removed, main() function, docstrings, exceptions, namings

--- gui/mvm_gui.py
+++ gui/mvm_gui.py
@@ -6,19 +6,28 @@ Runs the MVM GUI
 import sys
 import os
 import os.path
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMessageBox
-from PyQt5 import uic
+from PyQt5 import QtCore, QtWidgets
 
 import yaml
 
 from mainwindow import MainWindow
-from communication.esp32serial import ESP32Serial
+from communication.esp32serial import ESP32Serial, ESP32Exception
 from communication.fake_esp32serial import FakeESP32Serial
 from messagebox import MessageBox
 
 
 def connect_esp32(config):
+    """
+    Establish a serial connection with the ESP32.
+
+    arguments:
+    - config: a dictionary like representing the program configuration.
+
+    returns: a valid ESP32Serial object if connection has been
+    established, a FakeESP32Serial object if has been requested to run the
+    software mockup, 'None' on error.
+    """
+
     try:
         if 'fakeESP32' in sys.argv:
             print('******* Simulating communication with ESP32')
@@ -29,24 +38,28 @@ def connect_esp32(config):
             err_msg = "Cannot communicate with port %s" % config['port']
             esp32 = ESP32Serial(config)
             esp32.set("wdenable", 1)
-    except Exception as error:
+    except ESP32Exception as error:
         msg = MessageBox()
-        fn = msg.critical("Do you want to retry?",
-                          "Severe hardware communication error",
-                          str(error) + err_msg, "Communication error",
-                          {msg.Retry: lambda: connect_esp32(config),
-                           msg.Abort: lambda: None})
-        return fn()
+        answer = msg.critical("Do you want to retry?",
+                              "Severe hardware communication error",
+                              str(error) + err_msg, "Communication error",
+                              {msg.Retry: lambda: connect_esp32(config),
+                               msg.Abort: lambda: None})
+        return answer()
 
     return esp32
 
 
-if __name__ == "__main__":
+def main():
+    """
+    Main function.
+    """
+
     base_dir = os.path.dirname(__file__)
     settings_file = os.path.join(base_dir, 'default_settings.yaml')
 
-    with open(settings_file) as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
+    with open(settings_file) as fsettings:
+        config = yaml.load(fsettings, Loader=yaml.FullLoader)
     print('Config:', yaml.dump(config), sep='\n')
 
     app = QtWidgets.QApplication(sys.argv)
@@ -54,7 +67,7 @@ if __name__ == "__main__":
     esp32 = connect_esp32(config)
 
     if esp32 is None:
-        exit(-1)
+        sys.exit(-1)
 
     watchdog = QtCore.QTimer()
     watchdog.timeout.connect(esp32.set_watchdog)
@@ -64,3 +77,7 @@ if __name__ == "__main__":
     window.show()
     app.exec_()
     esp32.set("wdenable", 0)
+
+
+if __name__ == "__main__":
+    main()
